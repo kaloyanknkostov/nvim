091cd0ace171dbfe2f524647ceb755a09a23ca7b
kaloyan <kaloyanknkostov@gmail.com>
2025-10-28 17:51:40 +0200

    autopairs added

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -1,13 +1,12 @@
 vim.pack.add({
 	{ src = "https://github.com/rcarriga/nvim-notify" },
-	{ src = "https://github.com/altermo/ultimate-autopair.nvim" },
+	{ src = "https://github.com/windwp/nvim-autopairs" },
 })
 local notify = require("notify")
-
 vim.notify = notify
-
 notify.setup({
 	stages = "fade_in_slide_out",
 	timeout = 3000,
 	background_colour = "#000000",
 })
+require("nvim-autopairs").setup({})
